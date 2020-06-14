9491c473af1a91c4ed04148baabc505a767f2872
shariar-007 <uiu.151007@gmail.com>
2020-06-15 02:59:54 +0600

    data frame indexing and query

--- Module2/data-frame.py
+++ Module2/data-frame.py
@@ -43,5 +43,59 @@ print(copy_df)
 del copy_df['Name'] #column delete
 print(copy_df)
 
-df['Location'] = None
+df['Location'] = None  #add an extra column
 print(df)
+
+# =============================================================================
+# Dataframe Indexing and Loading
+# =============================================================================
+costs = df['Cost']
+print(costs)
+
+costs+=2
+print(costs)
+
+print(df)
+
+df = pd.read_csv('olympics.csv')
+print(df.head())
+
+df = pd.read_csv('olympics.csv', index_col = 0, skiprows=1)
+print(df.head())
+
+print(df.columns)
+
+for col in df.columns:
+    if col[:2]=='01':
+        df.rename(columns={col:'Gold' + col[4:]}, inplace=True)
+    if col[:2]=='02':
+        df.rename(columns={col:'Silver' + col[4:]}, inplace=True)
+    if col[:2]=='03':
+        df.rename(columns={col:'Bronze' + col[4:]}, inplace=True)
+    if col[:1]=='№':
+        df.rename(columns={col:'#' + col[1:]}, inplace=True) 
+                           
+print(df.head())
+
+# =============================================================================
+# Querying a DataFrame
+# =============================================================================
+
+print(df['Gold'] > 0)
+only_gold = df.where(df['Gold'] > 0)
+print(only_gold.head())
+
+print(only_gold['Gold'].count())
+print(df['Gold'].count())
+#print(df.count())
+
+only_gold = only_gold.dropna()
+print(only_gold.head())
+
+only_gold = df[df['Gold'] > 0]
+print(only_gold.head())
+
+print(len(df[(df['Gold'] > 0) | (df['Gold.1'] > 0)]))
+
+print(df[(df['Gold.1'] > 0) & (df['Gold'] == 0)])
+print(len(df[(df['Gold.1'] > 0) & (df['Gold'] == 0)]))
