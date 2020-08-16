6eb5ca6fc93f51ec446f375e09e86e500e9eb62c
shariar-007 <uiu.151007@gmail.com>
2020-08-17 02:55:56 +0600

    add visulization in question 13 of assignment 3

--- Module3/Assignment-3/problem-13.py
+++ Module3/Assignment-3/problem-13.py
@@ -7,6 +7,7 @@ Created on Mon Aug 17 02:47:54 2020
 
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 
 # =============================================================================
 # code of problem-1 for problem 3
@@ -71,3 +72,17 @@ def answer_thirteen():
     return Top15['PopEst'].apply(lambda x: '{0:,}'.format(x)).astype(str)
     # Adds in comma separators.
 print (answer_thirteen())
+
+############################################################################
+def plot_optional():
+    Top15 = final_df
+    ax = Top15.plot(x='Rank', y='% Renewable', kind='scatter', 
+                    c=['#e41a1c','#377eb8','#e41a1c','#4daf4a','#4daf4a','#377eb8','#4daf4a','#e41a1c',
+                       '#4daf4a','#e41a1c','#4daf4a','#4daf4a','#e41a1c','#dede00','#ff7f00'], 
+                    xticks=range(1,16), s=6*Top15['2014']/10**10, alpha=.75, figsize=[16,6]);
+    for i, txt in enumerate(Top15.index):
+        ax.annotate(txt, [Top15['Rank'][i], Top15['% Renewable'][i]], ha='center')
+    print("This is an example of a visualization that can be created to help understand the data. \
+This is a bubble chart showing % Renewable vs. Rank. The size of the bubble corresponds to the countries' \
+2014 GDP, and the color corresponds to the continent.")
+print(plot_optional())
